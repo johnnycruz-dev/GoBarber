04e352cdf25ab02e2af566ea334376b7fffe65be
Johnny Cruz <johnny@vermais.com>
2020-08-14 17:51:14 -0300

    Estruturando o código em DDD

---
+++ back-end/src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,41 @@
+import { hash } from 'bcryptjs';
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import User from '../infra/typeorm/entities/User';
+import IUsersRepository from '../repositories/IUsersRepository';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+@injectable()
+class CreateUserService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute({ name, password, email }: IRequest): Promise<User> {
+    const checkUsersExists = await this.usersRepository.findByEmail(email);
+
+    if (checkUsersExists) {
+      throw new AppError('Email address already used.');
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = await this.usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return user;
+  }
+}
+
+export default CreateUserService;
